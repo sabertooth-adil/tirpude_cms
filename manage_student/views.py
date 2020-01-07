d120fb3e078daf7985239f071ad587a6942ba471
sabertooth-adil <adil@sabertoothtech.in>
2020-01-07 12:47:55 +0530

    add venv/ in gitignore

--- manage_student/views.py
+++ manage_student/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+import random
+from django.conf import settings
+from django.core.mail import EmailMessage
+import base64
+import datetime
+import traceback
 # Create your views here.
