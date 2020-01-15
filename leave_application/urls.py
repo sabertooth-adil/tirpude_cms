bfcac738691d2072529429ef5f05a1f91af03ce0
Anagha Joshi <anagha.joshi52@gmail.com>
2020-01-15 17:17:19 +0530

    leave application optimized code

--- leave_application/urls.py
+++ leave_application/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('leave-application-submit/', views.leave_application_submit, name='leave_application_submit'),
     path('approve-disapprove-application/', views.approve_disapprove_application,
          name='approve_disapprove_application'),
-    path('view-leave-application-detail/', views.view_leave_application_detail, name='view_leave-application_detail'),
+    path('view-leave-application-detail/', views.view_leave_application_detail, name='view_leave_application_detail'),
     path('leave-application-detail/', views.get_leave_application_detail, name='leave_application_detail'),
     path('filter-applications/', views.filter_leave_applications, name='filter_leave_applications'),
     path('delete-leave-application/', views.delete_leave_application, name='delete_leave_application'),
